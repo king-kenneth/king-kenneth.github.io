dd86da40393615d3cc574cd4bae4c943bb550a7e
kenneth <king-kenneth@users.noreply.github.com>
2019-02-27 15:20:45 -0600

    wow

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -5,38 +5,50 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let directionX = 25;
-let directionY = 25;
-let colour;
+
+let x = 25;
+let y = 25;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  
-  fill(0);
-  rect(directionX, directionY, 50, 50);
+//background(255);
   checkingForKey();
+  mouseboy();
+ 
+  
 }
 
 function checkingForKey() {
+  fill(0);
+  rect(x, y, 50, 50);
   if (keyIsPressed) {
-    if (keyCode === LEFT_ARROW) {
-      directionX -= 10;
+    if (keyCode === LEFT_ARROW && x  > 0) {
+      x -= 10;
     } 
-    if (keyCode === RIGHT_ARROW) {
-      directionX += 10;
+    if (keyCode === RIGHT_ARROW && x < width/2) {
+      x += 10;
     }
-    if (keyCode === UP_ARROW) {
-      directionY -= 10;
+    if (keyCode === UP_ARROW && y  > 0) {
+      y -= 10;
     } 
-    if (keyCode === DOWN_ARROW) {
-      directionY += 10;
+    if (keyCode === DOWN_ARROW && y < height - 50) {
+      y += 10;
     }
     if (keyCode === CONTROL) {
       background(255);
     }
-    if 
+    
+    
   }
  
 }
+function mouseboy(){
+  if (mouseX > width/2);
+    
+  else;
+    rect(mouseX, mouseY, 25, 25);
+
+}
+ 
