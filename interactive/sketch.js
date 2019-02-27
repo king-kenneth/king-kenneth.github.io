0c1f65378fee5550ea817765d2974b9ffced3ce2
kenneth <king-kenneth@users.noreply.github.com>
2019-02-27 14:27:17 -0600

    y
    k

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -5,12 +5,38 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let directionX = 25;
+let directionY = 25;
+let colour;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  ellipse(mouseX, mouseY, 30, 30);
+  
+  fill(0);
+  rect(directionX, directionY, 50, 50);
+  checkingForKey();
+}
+
+function checkingForKey() {
+  if (keyIsPressed) {
+    if (keyCode === LEFT_ARROW) {
+      directionX -= 10;
+    } 
+    if (keyCode === RIGHT_ARROW) {
+      directionX += 10;
+    }
+    if (keyCode === UP_ARROW) {
+      directionY -= 10;
+    } 
+    if (keyCode === DOWN_ARROW) {
+      directionY += 10;
+    }
+    if (keyCode === CONTROL) {
+      background(255);
+    }
+    if 
+  }
+ 
 }
