a78959ef6fb8d591c4b9dd8960a0770cbb49a8e6
kenneth <king-kenneth@users.noreply.github.com>
2019-03-20 15:05:30 -0600

    kkkkkk

--- fire water ice/sketch.js
+++ fire water ice/sketch.js
@@ -4,6 +4,7 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
 let user = prompt("Hello User, What is your name?");
 let computerChoice;
 let whereAmI= start;
@@ -20,7 +21,7 @@ function setup() {
 
 function draw() {
 
-
+  menu()
   secret();
   textSize(32);
   text(computerChoice,10, 10, 700, 800);
@@ -31,34 +32,42 @@ function computerPicksElement() {
   computerChoice = Math.random();
   if (0 < computerChoice && computerChoice <= 0.33) {
     computerChoice = "fire";
-  } else if (0.34 < computerChoice && computerChoice < 0.66) { 
+  }
+  else if (0.34 < computerChoice && computerChoice < 0.66) { 
     computerChoice = "water";
-  } else {
+  }
+  else {
     computerChoice = "ice"; 
   }
 }
 
 function menu() {
-  while (whereAmI= start){
-  if (overMenuButton()) {
-    fill('gray');
-  } else {
-    fill('black');
+  while (whereAmI === start){  
+    if overMenuButton() {
+      fill("gray");
+    }
+    else {
+      fill("black");
+    }
+    
   }
-  rect(windowWidth/2 - 100, windowHeight/2 -50, 200, 100);
-  fill(255);
-  textSize(40);
-  text("START", windowWidth/2-60, windowHeight/2+10);
-}
+    rect(windowWidth/2 - 100, windowHeight/2 -50, 200, 100);
+    fill(255);
+    textSize(40);
+    text("START", windowWidth/2-60, windowHeight/2+10);
+  
 }
 
 function overMenuButton() {
-  return mouseX > windowWidth/2 - 100 && mouseX < windowWidth/2 + 100 && mouseY > windowHeight/2 -50 && mouseY < windowHeight/2 + 50;
+  return mouseX > windowWidth/2 - 100 &&
+   mouseX < windowWidth/2 + 100 &&
+    mouseY > windowHeight/2 -50 &&
+     mouseY < windowHeight/2 + 50;
 }
 
 function mousePressed() {
-  if overMenuButton(){
-    whereAmI = game;
+  if overMenuButton() {
+    whereAmI == game;
     }
   }
 
